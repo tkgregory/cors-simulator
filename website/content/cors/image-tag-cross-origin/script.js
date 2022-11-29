ea84718a8e031c5921622b18f1ed0705d11ed37a
Tom Gregory <t.k.gregory@gmail.com>
2022-11-29 11:35:50 +0000

    Centre image.

--- website/content/cors/image-tag-cross-origin/script.js
+++ website/content/cors/image-tag-cross-origin/script.js
@@ -1,6 +1,7 @@
 registerDynamicContent(function() {
     const image = new Image();
     image.src = "https://assets.cors-examples.tomgregory.com/waterfall.jpg";
+    image.className = "centred"
     image.onload = function () {
         $("#dynamic").append(image);
         udpateForSuccess(new URL(image.src).origin);
